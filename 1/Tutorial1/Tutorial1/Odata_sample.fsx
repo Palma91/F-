a1db0cf899c266537837905d5f76925de7ccf1e9
Alena Makrushina <Alena.Makrushina@sidenis.com>
2016-02-11 18:05:49 +0300

    3

---
+++ 1/Tutorial1/Tutorial1/Odata_sample.fsx
@@ -0,0 +1,18 @@
+﻿#r "packages/FSharp.Data.2.2.5/lib/net40/FSharp.Data.dll"
+#r "FSharp.Data.TypeProviders.dll"
+
+
+open System
+open System.Security
+open FSharp.Data
+open Microsoft.FSharp.Data.TypeProviders
+
+
+
+[<Literal>]
+let apiUrl  = "http://services.odata.org/Northwind/Northwind.svc/"
+
+type Northwind = ODataService<apiUrl>
+
+let db = Northwind.GetDataContext()
+let fullContext = Northwind.ServiceTypes.NorthwindEntities()
---
+++ 1/Tutorial1/Tutorial1/Odata_sm.fsx
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+[<Literal>]
+let apiUrl  = "http://demo-cd.systemorph.local/api/Policies"
+
+type NAVService = ODataService<apiUrl>
+
+db.DataContext.SendingRequest.Add (fun eventArgs -> printfn "Requesting %A" eventArgs.Request.RequestUri)
+
+
+let nav = NAVService.GetDataContext()
+nav.Credentials <- System.Net.CredentialCache.DefaultCredentials
+   
