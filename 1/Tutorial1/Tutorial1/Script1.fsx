54683de2ac3d485601c24fc9cb2e794146e268d3
palma91 <alena.makrushina@sidenis.com>
2016-02-11 05:43:12 +0300

    2

--- 1/Tutorial1/Tutorial1/Script1.fsx
+++ 1/Tutorial1/Tutorial1/Script1.fsx
@@ -4,4 +4,9 @@ open FSharp.Data
 
 
 let wb = WorldBankData.GetDataContext()
+wb.Countries.Afghanistan.CapitalCity
+wb.Countries.Afghanistan.Indicators.``Death rate, crude (per 1,000 people)``.[2012]
 
+
+for countries in wb.Regions.``Euro area``.Countries do 
+printfn  "%s, %s" countries.Name countries.CapitalCity
